f79b5840faf48e4d3ae06e3e2ffa19f2e0bda3a2
github username <github email address>
2018-07-04 17:54:05 -0500

    bachelor commit

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,72 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  answer = []
+  data.each do |seas, cont_stats|
+    if seas == season
+      cont_stats.each do |item|
+        if item.values.last == "Winner"
+          answer << item.values[0].split[0]
+        end
+      end    
+    end
+  end
+  answer[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  answer = []
+  data.each do |seas, cont_stats|
+      cont_stats.each do |item|
+        if item.values[3] == occupation
+          answer << item.values[0]
+        end
+      end    
+    end
+  answer[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |seas, cont_stats|
+      cont_stats.each do |item| 
+        if item.values[2] == hometown
+          count +=1
+        end
+      end    
+    end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  answer = []
+  data.each do |seas, cont_stats|
+      cont_stats.each do |item| 
+        if item.values[2] == hometown
+          answer << item.values[3]
+        end
+      end    
+    end
+  answer[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |seas, cont_stats|
+    if seas == season
+      cont_stats.each do |item|
+        ages << item.values[1].to_f
+      end    
+    end
+  end
+  total = ages.reduce(:+)
+  len = ages.length.to_f
+  (total/len).round
 end
+
+
+
+
+
+
+
